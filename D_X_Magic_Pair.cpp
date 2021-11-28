df6463e6bc256b3e34cb062b6d17940ee62a7d1c
= <pandit.rohit0007@gmail.com>
2021-11-29 01:24:41 +0530

    first contest

---
+++ D_X_Magic_Pair.cpp
@@ -0,0 +1,41 @@
+#pragma GCC optimize("Ofast")
+#pragma GCC target("sse,sse2,sse3,ssse3,sse4,popcnt,abm,mmx,avx,avx2,fma")
+#pragma GCC optimize("unroll-loops")
+#include <algorithm>
+#include <chrono>
+#include <cmath>
+#include <complex>
+#include <fstream>
+#include <iomanip>
+#include <iostream>
+#include <list>
+#include <map>
+#include <queue>
+#include <random>
+#include <set>
+#include <stack>
+#include <string>
+#include <unordered_map>
+#include <unordered_set>
+#include <vector>
+#define endl "\n"
+
+using namespace std;
+
+typedef long long ll;
+typedef long double ld;
+typedef pair<int, int> pii;
+typedef pair<ll, ll> pll;
+typedef pair<double, double> pdd;
+typedef vector<ll> vl;
+typedef vector<int> vi;
+typedef vector<vector<int> > vvi;
+typedef vector<vector<ll> > vvl;
+
+int main() {
+  ios_base::sync_with_stdio(false);
+  cin.tie(nullptr);
+#ifdef D_DEBUG
+  freopen("input.txt", "r", stdin)
+#endif
+}
